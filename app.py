0cb06f303bbaa2ef9f14b7fec787ec11bb166a2a
nguyenhuuthanh-wed <nguyenhuuthanh2k6@gmail.com>
2025-10-21 23:18:27 +0700

    feat: Implement function to add a new task

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-# Danh sách để lưu các công việc
 tasks = []
 def add_task(task_name):
     """Thêm một công việc mới vào danh sách."""
     tasks.append(task_name)
     print(f"Đã thêm công việc: '{task_name}'")
 
-def list_task(task):
-    for i in range(len(task)):
-        print(f'{i+1}. {task[i]}')
+def list_task(tasks):
+    for i in tasks:
+        if i["completed"] == "True":
+            print(f'[x] {i["name"]}')
+        else:
+            print(f'[ ] {i["name"]}')
+
+def complete_task(task_index):
+    for i in range(len(tasks)):
+        print(f'{i+1}. {tasks[i]}')
+        if (task_index - 1 == i):
+            tasks[i][1] = "True"
 
 # --- Điểm bắt đầu của chương trình ---
 if __name__ == "__main__":
     print("Chào mừng đến với ứng dụng To-Do List!")
-    add_task("Học bài Git và GitHub")
-    add_task("Làm bài tập thực hành ở nhà")
+    add_task("Học bài GitHub")
+    add_task("Học bài Git")
+    for i in range(2):
+        name = input('Nhập tên công việc: ')
+        completed_input = input("Nhập trạng thái hoàn thành (True/False): ").strip().lower()
+        completed = "True" 
+        if completed_input == "true" else "False"
 
-
+        dic = {"name" : name, "completed" : completed}
+        add_task(dic)
+    
+    complete_task(1)
+    list_task(tasks)
